82ef4d91d7cd395d837cadd60d33e3f57cb6f20b
asus-desktop <asus-desktop@ASUS-DESKTOP>
2019-06-03 00:32:04 +0430

    SumStrategy changed

--- SumBigNumber/SumStrategy.cs
+++ SumBigNumber/SumStrategy.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace SumBigNumber
 {
+    /// <summary>
+    /// SumStrategy class provides an abstract view to implement different strategy for add(sum) numbers.
+    /// </summary>
     public abstract class SumStrategy : ICalculate
     {
         public abstract string Calculate(string[] stringNum);
