6623f9b29f4239a232ef96ebf7a473dd6c72ea46
asus-desktop <asus-desktop@ASUS-DESKTOP>
2019-06-03 11:09:39 +0430

    UnitTest

--- UnitTestBigNumberStrategy/UnitTest1.cs
+++ UnitTestBigNumberStrategy/UnitTest1.cs
@@ -1,13 +1,26 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SumBigNumber;
+using System.Collections.Generic;
 
 namespace UnitTestBigNumberStrategy
 {
     [TestClass]
     public class UnitTest1
     {
+        
         [TestMethod]
-        public void TestMethod1()
+        public void TestMultiAdd()
         {
+            // Arrange
+            var creator = new CalculationFactory();
+            ICalculate calculate = creator.FactoryMethod(CalculationCreator.OperationType.BigSum);            
+            List<string> mylist = new List<string>(new string[] { "121556550", "15589455452", "2254564555565552", "5554525455454554565" });
+                        
+            // Act
+            string multiResult = calculate.Calculate(mylist);
+
+            // Assert
+            Assert.AreEqual(multiResult, "5556780035721132119");
         }
     }
 }
